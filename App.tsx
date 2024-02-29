61765390cf5860049cee4a89e505c9c4428ca632
ivan <ivantrajanovski@gmail.com>
2024-02-29 11:32:06 +0100

    fetch data from api

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import LoginScreen from "./src/screens/LoginScreen";
 import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <PaperProvider>
-      <StarshipFeedScreen />
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <StarshipFeedScreen />
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/StarshipFeedScreen.tsx
+++ src/screens/StarshipFeedScreen.tsx
@@ -1,42 +1,66 @@
 import React from "react";
-import { StyleSheet, StatusBar, View, Text, FlatList } from "react-native";
-import { Card } from "react-native-paper";
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+  Text,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+import { Card, Button } from "react-native-paper";
 
-import data from "../../api/data.json";
+import { useStarships } from "~/hooks/useStarships";
 
 type ItemProps = {
   item: {
     name: string;
     model: string;
-    crew: string;
-    hyperdrive_rating: string;
+    manufacturer: string;
     cost_in_credits: string;
   };
 };
 
 const Item = ({ item }: ItemProps) => (
   <Card style={styles.card}>
-    <Card.Cover source={{ uri: "https://picsum.photos/seed/car/400/200" }} />
-    <Card.Title title={item.name} subtitle={item.model} />
+    {/* <Card.Cover source={{ uri: item.image }} /> */}
+    <Card.Title
+      title={item.name}
+      subtitle={item.model}
+      titleStyle={{ fontSize: 24 }}
+    />
     <Card.Content>
-      <Text>{`Crew: ${item.crew}`}</Text>
-      <Text>{`Hyperdrive Rating: ${item.hyperdrive_rating}`}</Text>
-      <Text>{`Cost in Credits: ${item.cost_in_credits}`}</Text>
+      <Text>{item.manufacturer}s</Text>
+      <Text style={{ fontSize: 20 }}>{`${item.cost_in_credits} credits`}</Text>
     </Card.Content>
+    <Card.Actions>
+      <Button mode="text"> BUY THIS SPACESHIP</Button>
+    </Card.Actions>
   </Card>
 );
 
 export const StarshipFeedScreen = () => {
+  const { isLoading, error, data } = useStarships();
+
+  if (isLoading) {
+    return <Text>Loading…</Text>;
+  }
+
+  if (error) {
+    return <Text>Something bad happened…</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerTitle}>Starships</Text>
+      <View style={styles.itemsContainer}>
         <FlatList
           data={data.results}
           renderItem={({ item }) => <Item item={item} />}
           keyExtractor={(item) => item.name.toString()}
+          showsVerticalScrollIndicator={false}
         />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -45,12 +69,16 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0, // only for Android to avoid status bar overlap
   },
-  headerContainer: {
+  itemsContainer: {
     paddingHorizontal: 20,
     marginTop: 20,
   },
+  headerTitle: {
+    padding: 16,
+    fontSize: 32,
+    fontWeight: "bold",
+  },
   item: {
-    // backgroundColor: "purple",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
@@ -59,6 +87,7 @@ const styles = StyleSheet.create({
     fontSize: 32,
   },
   card: {
-    marginVertical: 8,
+    margin: 8,
+    backgroundColor: "white",
   },
 });
